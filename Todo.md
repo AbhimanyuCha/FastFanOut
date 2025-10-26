163dc33190c6ccd82d09be87c124959662e4b975
AbhimanyuCha <abhimanyuc609@gmail.com>
2025-10-26 21:31:54 +0530

    refactor + redis impl

---
+++ Todo.md
@@ -0,0 +1,7 @@
+<!-- 
+
+1. Dockerize this 
+2. Write docker-compose
+3. Users are in-memory right now, move it to redis
+
+ -->
---
+++ server/redisClient.js
@@ -0,0 +1,18 @@
+const { createClient } = require("redis");
+
+const publisher = createClient({ url: "redis://localhost:6379" });
+const subscriber = createClient({ url: "redis://localhost:6379" });
+
+async function initRedis() {
+  try {
+    await publisher.connect();
+    await subscriber.connect();
+    console.log("✅ Redis publisher & subscriber connected");
+  } catch (err) {
+    console.error("❌ Redis connection error:", err);
+  }
+}
+
+initRedis();
+
+module.exports = { publisher, subscriber };
